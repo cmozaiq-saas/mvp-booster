81d24fe985d0ba456101bcdf12e7f33d95e5503b
cmozaiq-saas <ai.agent.builder@gmail.com>
2025-05-08 14:39:51 +0100

    great for uuid ordering

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,4 @@
 class ApplicationRecord < ActiveRecord::Base
   include SpreadsheetArchitect; primary_abstract_class
+  self.implicit_order_column = "created_at" # great when using uuid to by default order records
 end
